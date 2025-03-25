71061952aa7f1727ed84f2a508c992171db3a989
kakuro83 <kahicer@gmail.com>
2025-03-25 01:13:21 -0500

    Update Cromatografía.py

--- Cromatografía.py
+++ Cromatografía.py
@@ -147,9 +147,9 @@ if not hoja_ejercicio.empty:
         except:
             carga_proteina = 0
 
-        costos_acumulados = 0
-        tiempo_total_h = 0
-        for i in range(1, 5):
+costos_acumulados = 0
+tiempo_total_h = 0
+for i in range(1, 5):
             st.markdown(f"**Etapa {i}**")
             col1, col2, col3 = st.columns(3)
             with col1:
