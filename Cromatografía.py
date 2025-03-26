6afd54de6629643f6b83a1882a8fda4d2c7176af
kakuro83 <kahicer@gmail.com>
2025-03-25 22:35:19 -0500

    Update Cromatografía.py

--- Cromatografía.py
+++ Cromatografía.py
@@ -191,7 +191,8 @@ if proteina_seleccionada != "Seleccionar proteína":
                 # Tiempo y costo
                 tiempo_min = calcular_tiempo(carga, velocidad, corridas)
                 tiempo_h = tiempo_min / 60
-                costo_total = calcular_costo(costo_columna, corridas)
+                costo_operativo = valor_str = df_datos[df_datos["Parámetro"] == f"Costos fijos operativos (USD/h)"]["Valor"].values[0]
+                costo_total = calcular_costo(costo_columna, corridas) + tiempo_h * costo_operativo
 
                 # Acumular totales
                 costos_acumulados += costo_total
