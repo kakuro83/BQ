7261afb2dd26be840ea82c5c6fa94359de1ef3d2
kakuro83 <kahicer@gmail.com>
2025-03-26 12:56:20 -0500

    Update Cromatografía.py

--- Cromatografía.py
+++ Cromatografía.py
@@ -369,7 +369,16 @@ for i in range(1, 5):
             elif "lectina" in tecnica_lower:
                 sigla = "Lec"
             else:
-                sigla = tecnica.split()[0].upper()
+                # Buscar en df_purificacion la sigla entre paréntesis
+                fila_tec = df_purificacion[df_purificacion["Técnica"] == tecnica]
+                if not fila_tec.empty:
+                    tecnica_texto = fila_tec["Técnica"].values[0]
+                    if "(" in tecnica_texto and ")" in tecnica_texto:
+                        sigla = tecnica_texto.split("(")[-1].split(")")[0].strip()
+                    else:
+                        sigla = tecnica.split()[0].upper()
+                else:
+                    sigla = tecnica.split()[0].upper()
             codigo_etapas.append(f"{sigla},{corridas},{velocidad}")
 
 # Mostrar resultado
